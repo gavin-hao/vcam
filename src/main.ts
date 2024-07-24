5b5ccc9ae1ef9f93553111d1d1675be5bc37004d
haozhigang <haozhigang@huice.com>
2024-07-24 11:51:07 +0800

    xxxx

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-createApp(App).mount('#app').$nextTick(() => {
-  // Use contextBridge
-  window.ipcRenderer.on('main-process-message', (_event, message) => {
-    console.log(message, 9999)
-  })
-})
+import 'element-plus/dist/index.css';
+createApp(App)
+  .mount('#app')
+  .$nextTick(() => {
+    // Use contextBridge
+    window.ipcRenderer.on('main-process-message', (_event, message) => {
+      console.log(message, 9999);
+    });
+  });
