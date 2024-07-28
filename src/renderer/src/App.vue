839ad101f0eb3c591b4d948a12bc1f5c7cc6aacd
huskylengcb <649569822@qq.com>
2024-07-28 22:13:31 +0800

    xx

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <Camera></Camera>
+  <template v-if="overtime">
+    <div class="notice">已超出试用期，请联系研发人员</div>
+  </template>
+  <template v-else>
+    <Camera />
+  </template>
 </template>
+
 <script setup lang="ts">
-// import Versions from './components/Versions.vue';
-import Camera from './components/Camera.vue';
+  import { computed } from 'vue';
+  import Camera from './components/Camera.vue';
+  
+  const overtime = computed(() => {
+    return new Date() > new Date('2024-08-15')
+  })
 </script>
+
+<style scoped>
+.notice {
+  text-align: center;
+  height: 100%;
+  margin: auto;
+}
+</style>
